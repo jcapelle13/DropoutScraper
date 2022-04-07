9237553dc2496ff6faf5d7b988bf3f12563a3504
Jared Capelle <jcapelle13@gmail.com>
2022-04-07 01:15:23 -0400

    Add file sanitization and requirements.txt

--- stripper.py
+++ stripper.py
@@ -1,7 +1,13 @@
 from bs4 import BeautifulSoup
 import wget
+import os
+from sanitize_filename import sanitize
 
 filename = "file.html"
+dest_dir = "imgs"
+
+if not os.path.exists(dest_dir):
+    os.mkdir(dest_dir)
 
 html_file = open(filename, 'r')
 
@@ -13,7 +19,13 @@ for tag in soup.find_all('img'):
     url = tag['src']
     url = url.split('?')[0]
     filename = filename + "." + url.split('.')[-1]
-    wget.download(url, filename)
+    filename = sanitize(filename)
+    path = os.path.join(dest_dir, filename)
+    if os.path.exists(path):
+        print("Existing File found:", filename)
+    else:
+        print("Downloading", filename, "from", url)
+        wget.download(url, path)
 
 
 html_file.close()
