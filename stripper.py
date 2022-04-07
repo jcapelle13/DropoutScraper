18c305a2c400c7b93e903deafb771b2a4d6e793b
Jared Capelle <jcapelle13@gmail.com>
2022-04-07 01:20:06 -0400

    Add Comments

--- stripper.py
+++ stripper.py
@@ -3,12 +3,14 @@ import wget
 import os
 from sanitize_filename import sanitize
 
+#TODO: Make use command line params?
 filename = "file.html"
 dest_dir = "imgs"
 
 if not os.path.exists(dest_dir):
     os.mkdir(dest_dir)
 
+# Because i don't like with "with open() as f" blocks creating an indent
 html_file = open(filename, 'r')
 
 html = html_file.read()
@@ -17,7 +19,10 @@ soup = BeautifulSoup(html, 'html.parser')
 for tag in soup.find_all('img'):
     filename = tag['alt']
     url = tag['src']
+    # Strip off any extra query stuff, just get the file (limits scope admitedly)
     url = url.split('?')[0]
+    # Get file Extension from url end
+    #TODO: Add check for lack of extension?
     filename = filename + "." + url.split('.')[-1]
     filename = sanitize(filename)
     path = os.path.join(dest_dir, filename)
